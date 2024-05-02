cbf4d4d2cf44565001d4dcb2baeace59e19eed2d
noeliecomte <noelie.comte02@gmail.com>
2024-05-02 10:19:05 +0200

    code to get the trascription of a video

--- get_video_transcription.py
+++ get_video_transcription.py
@@ -2,5 +2,13 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 video_id = "R-lcz83U0WI"
 
-print(YouTubeTranscriptApi.get_transcript(video_id, languages=['fr']))
+#transcript=YouTubeTranscriptApi.get_transcript(video_id, languages=['fr'])
+#transcript_fulltxt = transcript.translate('fr').fetch()
+#print(transcript_fulltxt)
 
+srt = YouTubeTranscriptApi.get_transcript(video_id, languages=['fr'])
+
+txt=""
+for i in srt:
+    txt+=(i['text'])
+print(txt)
